9cf348de0ff7d4bddec6f1d9d421b07c2eea8365
PP <chinese1236@qq.com>
2021-11-23 15:40:06 +0800

    Update game.ts

--- cocos/core/game.ts
+++ cocos/core/game.ts
@@ -1,7 +1,7 @@
 /*
  Copyright (c) 2013-2016 Chukong Technologies Inc.
  Copyright (c) 2017-2020 Xiamen Yaji Software Co., Ltd.
-   
+    
  http://www.cocos.com
 
  Permission is hereby granted, free of charge, to any person obtaining a copy
