cb721ed7d3d051117d45161289fcf7558148218d
PP_Pro <chinese1236@qq.com>
2022-05-20 18:02:35 +0800

    sss

--- cocos/input/control.ts
+++ cocos/input/control.ts
@@ -1,14 +1,8 @@
 import { Vec2 } from '../core/math/vec2'
+import { Vec3 } from '../core/math/vec3';
 
 export abstract class InputControl<T> {
     abstract readValue(): T;
-}
-
-export class BoolControl extends InputControl<boolean> {
-    readValue(): boolean {
-        throw new Error("Method not implemented.");
-    }
-    
 }
 
 export class AxisControl extends InputControl<number> {
@@ -19,6 +13,13 @@ export class AxisControl extends InputControl<number> {
 
 export class Vec2Control extends InputControl<Vec2> {
     readValue(): Vec2 {
+        throw new Error('Method not implemented.');
+    }
+
+}
+
+export class Vec3Control extends InputControl<Vec3> {
+    readValue(): Vec3 {
         throw new Error('Method not implemented.');
     }
 
