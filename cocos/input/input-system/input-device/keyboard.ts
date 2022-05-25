0601011df37f394e723607758030216b89e49d93
PP_Pro <chinese1236@qq.com>
2022-05-26 01:10:14 +0800

    update

--- cocos/input/input-system/input-device/keyboard.ts
+++ cocos/input/input-system/input-device/keyboard.ts
@@ -1,5 +1,8 @@
 import { AxisControl } from '../control';
 
 export class Keyboard1 {
+    public static keyW: AxisControl;
     public static keyA: AxisControl;
+    public static keyS: AxisControl;
+    public static keyD: AxisControl;
 }
