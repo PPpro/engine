dbe6c001941ff97715db6c4e1f8c34fbdb6cf49a
PP_Pro <chinese1236@qq.com>
2023-08-10 18:36:56 +0800

    support ambient type module

---
+++ modules/ambient-type/src/index.d.ts
@@ -0,0 +1,29 @@
+// default ambient types
+/// <reference path="../lib/consts.d.ts"/>
+/// <reference path="./types/editor-extends.d.ts"/>
+/// <reference path="./types/globals.d.ts"/>
+/// <reference path="./types/jsb.d.ts"/>
+/// <reference path="./types/webGL.extras.d.ts"/>
+/// <reference path="./types/webGL2.extras.d.ts"/>
+
+// node_modules type
+/// <reference path="../../../node_modules/@cocos/cannon/CANNON.d.ts"/>
+/// <reference path="../../../node_modules/@types/jest/index.d.ts"/>
+
+// pal types
+/// <reference path="./types/pal/audio.d.ts"/>
+/// <reference path="./types/pal/env.d.ts"/>
+/// <reference path="./types/pal/input-box.d.ts"/>
+/// <reference path="./types/pal/input.d.ts"/>
+/// <reference path="./types/pal/minigame.d.ts"/>
+/// <reference path="./types/pal/pacer.d.ts"/>
+/// <reference path="./types/pal/screen-adapter.d.ts"/>
+/// <reference path="./types/pal/system-info.d.ts"/>
+/// <reference path="./types/pal/wasm.d.ts"/>
+
+// external types
+/// <reference path="../../../native/external/emscripten/external-wasm.d.ts"/>
+/// <reference path="../../../native/external/emscripten/bullet/bullet.d.ts"/>
+/// <reference path="../../../native/external/emscripten/physx/physx.d.ts"/>
+/// <reference path="../../../native/external/emscripten/webgpu/webgpu.d.ts"/>
+/// <reference path="../../../native/external/emscripten/spine/spine.d.ts"/>
